28dfca21c551c82befda870c0ebfc2981f434036
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-09-01 23:40:59 -0500

    agregué el mapa y acomodé estilos

--- ip-address-tracker/src/App.js
+++ ip-address-tracker/src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
+import Map from "./components/Map";
+import L from 'leaflet';
 
 import './App.css';
+import "leaflet/dist/leaflet.css"
 
 function App() {
   return (
     <>
     <Header />
+    <Map />
     </>
   );
 }
--- ip-address-tracker/src/components/Header.js
+++ ip-address-tracker/src/components/Header.js
@@ -1,24 +1,52 @@
 import pattern from '../images/pattern.png';
 import arrow from '../images/icon-arrow.svg';
-import { Container, InputGroup, Button, FormControl } from 'react-bootstrap'
+import { InputGroup, Button, FormControl, Card } from 'react-bootstrap'
 
 function Header() {
     return (
         <>
-        <img src={ pattern } alt="bg" />
-        <Container className="container">
-            <h2 className="titulo">IP Address Tracker</h2>
-            <InputGroup className="mb-3">
-                <FormControl className="form"
-                placeholder="Search for any IP address or domain"
-                // aria-label="Recipient's username"
-                // aria-describedby="basic-addon2"
-                />
-                <Button variant="dark" id="button-addon2">
-                <img src={ arrow } alt="bg" />
-                </Button>
-            </InputGroup>
-        </Container>
+        <header className="header">
+            <img src={ pattern } alt="bg" className="bg"/>
+            <div className="container">
+                <h2 className="titulo">IP Address Tracker</h2>
+                <InputGroup className="mb-3">
+                    <FormControl className="form"
+                    placeholder="Search for any IP address or domain"
+                    // aria-label="Recipient's username"
+                    // aria-describedby="basic-addon2"
+                    />
+                    <Button variant="dark" id="button-addon2">
+                    <img src={ arrow } alt="bg" />
+                    </Button>
+                </InputGroup>
+            </div>  
+        </header>
+        <Card className="d-flex flex-row">
+            <Card.Body className="col-2 p-3 bg-primary">
+                <Card.Title>IP Address</Card.Title>
+                <Card.Text>
+                192.212.173.101
+                </Card.Text>
+            </Card.Body>
+            <Card.Body className="col-2 p-3 bg-warning">
+                <Card.Title>Location</Card.Title>
+                <Card.Text>
+                Brooklyn, NY
+                </Card.Text>
+            </Card.Body>
+            <Card.Body className="col-2 p-3 bg-info">
+                <Card.Title>Timezone</Card.Title>
+                <Card.Text>
+                UTC-05:00
+                </Card.Text>
+            </Card.Body>
+            <Card.Body className="col-2 p-3 bg-danger">
+                <Card.Title>ISP</Card.Title>
+                <Card.Text>
+                Space X Starlink
+                </Card.Text>
+            </Card.Body>
+        </Card> 
         </>   
     )
 }
